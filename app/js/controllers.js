b15ee82f7c20368b2aef69469b5fcb567dd09eaa
Travis Hunter <thunter@optaros.com>
2013-12-01 17:48:45 -0500

    Minor styling changes. Added ability to specify video domain and id to dynamically create embed html for video detail pages. Created anonymous function in detail controller to dynamically create iframe when video_domain and video_id are provided in json data.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,8 +20,20 @@ portfolioControllers.controller('ProjectListCtrl', ['$scope', 'Portfolio',
 portfolioControllers.controller('ProjectDetailCtrl', ['$scope', 'Portfolio', '$sce', '$routeParams',
   function ($scope, Portfolio, $sce, $routeParams) {
     $scope.project = Portfolio.get({dataId: $routeParams.projectId}, function (project) {
-      if (project.type == 'video')
-        $scope.embedHtml = project.embedHtml ? $sce.trustAsHtml(project.embedHtml) : '';
+      // A bit more work to do for videos
+      if (project.type == 'video') {
+        var embedHtml = project.embed_html ? project.embed_html : (function (data) {
+          var videoSrc = '';
+          var embed = '<iframe width="1000" height="562" src="{video_src}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+          if (data.video_domain == 'youtube') {
+            videoSrc = '//www.youtube.com/embed/{video_id}?autohide=1&fs=1&autoplay=0&iv_load_policy=3&rel=0&modestbranding=1&showinfo=0&hd=1';
+          } else if (data.video_domain == 'vimeo') {
+            videoSrc = '//player.vimeo.com/video/{video_id}?title=0&portrait=0&byline=0';
+          }
+          return embed.replace('{video_src}', videoSrc.replace('{video_id}', data.video_id));
+        })(project);
+        $scope.embedHtml = $sce.trustAsHtml(embedHtml);
+      }
     });
   }
 ]);
