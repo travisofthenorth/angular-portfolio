b7dded328cce51e2bba144a2eb6423f929d0896d
Travis Hunter <thunter@optaros.com>
2014-01-01 18:17:29 -0500

    Added config vars to email script

--- app/send.php
+++ app/send.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'swift/lib/swift_required.php';
 
+$GMAIL_USERNAME = 'GMAIL_USERNAME';
+$GMAIL_PASSWORD = 'GMAIL_PASSWORD';
+$MY_EMAIL_ADDRESS = 'MY_EMAIL_ADDRESS';
+
 // We don't need no GETs!
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header('HTTP/1.0 404 Not Found');
@@ -23,15 +27,15 @@ if ($request->name == null || $request->email == null || $request->body == null)
 
 // Set your username/password here! Two-step authentication seems to be an issue if enabled.
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
-  ->setUsername('GMAIL_USERNAME')
-  ->setPassword('GMAIL_PASSWORD');
+  ->setUsername($GMAIL_USERNAME)
+  ->setPassword($GMAIL_PASSWORD);
 
 $mailer = Swift_Mailer::newInstance($transport);
 
 // Set the To: field here to the address that needs to receive the emails!
 $message = Swift_Message::newInstance('Portfolio: Message from ' . $request->name)
   ->setFrom(array($request->email => $request->name))
-  ->setTo(array('MY_EMAIL_ADDRESS'))
+  ->setTo(array($MY_EMAIL_ADDRESS))
   ->setBody($request->body);
 
 $result = $mailer->send($message);
